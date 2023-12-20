2ba01a0629188938bbf37b69e695fe682b83c9da
root <alanborges2020@outlook.comm>
2023-12-20 13:56:54 -0300

    Versionando ProjMod19

---
+++ src/com/TarefaReflection/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.TarefaReflection;
+
+import java.lang.annotation.Annotation;
+
+/**
+ *
+ * @author alan.menezes
+ */
+public class Main {
+    
+   
+    public static void main(String[] args) {
+         Class clazz = Tabela.class;
+         System.out.println(clazz.getClass() + "\n" + clazz);
+              
+    }   
+}
